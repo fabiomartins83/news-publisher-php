c69d602eb128a20c7e322ffb59b37946237489fc
Fabio de Almeida Martins <fabio.martins.01@hotmail.com>
2026-04-15 06:04:35 -0300

    Adicionando foreign keys no banco de dados

--- api.php
+++ api.php
@@ -15,7 +15,7 @@ set_exception_handler(function($e) {
 });
 
 $db = new SQLite3("materias.sqlite");
-
+$db->exec("PRAGMA foreign_keys = ON");
 $db->exec("PRAGMA journal_mode = WAL;");
 $db->exec("PRAGMA busy_timeout = 5000;");
 
